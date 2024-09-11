396a4b8386d86ba1a95a297b55db6d8625a7ae82
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-09-11 11:32:50 +0700

    triggering linting

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log("test");
+const names = ["test"];
+for (const name of names) {
+	console.log(`item is: ${name}`);
+}
+
+const items = [""];
+
+for (const item of items) {
+	console.log(item);
+}
