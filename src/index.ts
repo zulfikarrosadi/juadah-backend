2b71dd6a2ae66cb5764fefeb9e479de20d3ab47b
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-09-12 09:31:16 +0700

    chore: format index.ts using prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import cookieParser from "cookie-parser";
-import express from "express";
-import routes from "./routes";
-import "dotenv/config";
-import cors from "cors";
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import routes from './routes'
+import 'dotenv/config'
+import cors from 'cors'
 
-const app = express();
-const port = process.env.SERVER_PORT;
+const app = express()
+const port = process.env.SERVER_PORT
 
 app.use(
-	cors({
-		credentials: true,
-		origin: ["http://localhost:5173"],
-		allowedHeaders: ["Content-Type", "Authorization"],
-	}),
-);
-app.use(express.json());
-app.use(cookieParser());
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+)
+app.use(express.json())
+app.use(cookieParser())
 
 app.listen(port, () => {
-	routes(app);
-	console.info(`Server running at port ${port}`);
-});
+  routes(app)
+  console.info(`Server running at port ${port}`)
+})
