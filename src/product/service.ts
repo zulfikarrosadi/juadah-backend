f5619c80c232a6357d2297b5c9b23e8b99140bcc
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-09-14 23:19:32 +0700

    add product service
    add createProduct method

---
+++ src/product/service.ts
@@ -0,0 +1,57 @@
+import type { CreateProduct, Product } from './schema'
+
+interface ProductRepository {
+  createProduct(
+    data: CreateProduct,
+  ): Promise<{ id: number; affectedRows: number }>
+  getProductById(id: number): Promise<Product>
+}
+
+export type ApiResponse<Data> =
+  | { status: 'success'; data: Record<string, Data> }
+  | {
+      status: 'fail'
+      errors: {
+        code: number
+        message: string
+        details?: Record<string, string>
+      }
+    }
+
+class ProductService {
+  constructor(public repo: ProductRepository) {}
+
+  createProduct = async (
+    data: CreateProduct,
+  ): Promise<ApiResponse<Product>> => {
+    try {
+      const newProduct = await this.repo.createProduct(data)
+      const result = await this.repo.getProductById(newProduct.id)
+
+      return {
+        status: 'success',
+        data: {
+          products: {
+            id: result.id,
+            name: result.name,
+            description: result.description,
+            price: result.price,
+            image: result.image,
+          },
+        },
+      }
+    } catch (error) {
+      console.log('product service error: ', error)
+
+      return {
+        status: 'fail',
+        errors: {
+          code: error.code,
+          message: error.message || error,
+        },
+      }
+    }
+  }
+}
+
+export default ProductService
