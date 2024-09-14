5140d6e19b232ea5e4df1c569241b16ffc5b0126
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-09-14 23:21:59 +0700

    add product handler
    add createProduct method

---
+++ src/product/handler.ts
@@ -0,0 +1,28 @@
+import type { Request, Response } from 'express'
+import type { CreateProduct, Product } from './schema'
+import type { ApiResponse } from './service'
+
+interface ProductService {
+  createProduct(data: CreateProduct): Promise<ApiResponse<Product>>
+}
+
+class ProductHandler {
+  constructor(public service: ProductService) {}
+
+  createProduct = async (
+    req: Request<
+      Record<string, unknown>,
+      Record<string, unknown>,
+      CreateProduct
+    >,
+    res: Response<ApiResponse<Product>>,
+  ) => {
+    const result = await this.service.createProduct(req.body)
+    if (result.status === 'fail') {
+      return res.status(result.errors.code).json(result)
+    }
+    return res.status(201).json(result)
+  }
+}
+
+export default ProductHandler
