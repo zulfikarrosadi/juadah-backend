0c1eaee600fe1a40bf7b5d726a2ebbc5fb988cf8
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-09-15 14:46:35 +0700

    feat(upload image): add middlware to wrap multer to get better error handler

---
+++ src/middlewares/formDataParser.ts
@@ -0,0 +1,18 @@
+import type { NextFunction, Request, Response } from 'express'
+
+export default function formDataParse(multer: any) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    multer(req, res, async (err: any) => {
+      if (err) {
+        return res.status(400).json({
+          status: 'fail',
+          errors: {
+            code: 400,
+            message: err.message,
+          },
+        })
+      }
+      return next()
+    })
+  }
+}
