474dd9b44c64b5de2c50aff4c75811748536a646
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-20 11:28:40 +0700

    feat(type response): add new flexible type response

--- src/schema.ts
+++ src/schema.ts
@@ -11,6 +11,17 @@ type ApiResponse<Data> =
       }
     }
 
+export type ApiResponseV2<Data = Record<string, any>> =
+  | { status: 'success'; data: Data }
+  | {
+      status: 'fail'
+      errors: {
+        code: number
+        message: string
+        details?: Record<string, any>
+      }
+    }
+
 export type UserInToken = {
   fullname: string
   email: string
