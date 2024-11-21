b520299931a63174ae83f9fed97e192c79fc8011
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-21 15:38:33 +0700

    fix(docs): tweak swagger so it can shows in vercel

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ const app = express()
 import swaggerDocument from './openapi.json'
 import routes from './routes'
 
+const SWAGGER_CSS_URL =
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+
 const port = process.env.SERVER_PORT
 
 BigInt.prototype.toJSON = function () {
@@ -18,7 +21,15 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(sanitizeInput)
-app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use(
+  '/api/docs',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument, {
+    customCssUrl: SWAGGER_CSS_URL,
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  }),
+)
 app.use('/api/', routes)
 app.listen(port, () => {
   console.info(`Server running at port ${port}`)
